94daec0f5137a8e70da66ad5f5c46a592eb5d096
Simon Moos <simon.moos95@gmail.com>
2015-04-30 00:26:49 +0200

    Started scratchpading the game

--- Sokobhaskel.hs
+++ Sokobhaskel.hs
@@ -3,6 +3,8 @@ module Main where
 import Prelude hiding (Either(..))
 
 
+type Vec2 = (Int, Int)
+
 data Input = Up
            | Down
            | Left
@@ -19,43 +21,48 @@ getInput = do
     'd'       -> return Right
     otherwise -> getInput
 
-type Vec2 = (Int, Int)
+add :: Vec2 -> Input -> Vec2
+add (x,y) Up    = (x, y - 1)
+add (x,y) Down  = (x, y + 1)
+add (x,y) Left  = (x - 1, y)
+add (x,y) Right = (x + 1, y)
 
 data SokobanBoard = SokobanBoard { sbSize    :: Vec2   -- size of board
+                                 , sbPlayer  :: Vec2   -- player position
                                  , sbWalls   :: [Vec2] -- wall positions
                                  , sbBoxes   :: [Vec2] -- box positions
                                  , sbTargets :: [Vec2] -- target position
-                                 , sbPlayer  :: Vec2   -- player position
                                  } deriving (Eq)
+
+emptySokobanBoard :: SokobanBoard
+emptySokobanBoard = SokobanBoard { sbSize    = (0, 0)
+                                 , sbPlayer  = (0, 0)
+                                 , sbWalls   = []
+                                 , sbBoxes   = []
+                                 , sbTargets = []
+                                 }
+
+isTile :: (SokobanBoard -> [Vec2]) -> SokobanBoard -> Vec2 -> Bool
+isTile tileListProvider sb coord = coord `elem` tileListProvider sb
+
+isWall   = isTile sbWalls
+isBox    = isTile sbBoxes
+isTarget = isTile sbTargets
 
 instance Show SokobanBoard where
   show = showBoard
 
-readBoard :: String -> SokobanBoard
-readBoard = undefined
-
 showBoard :: SokobanBoard -> String
 showBoard = undefined
+
+readBoard :: String -> SokobanBoard
+readBoard = undefined
 
 
 
 main :: IO ()
 main = do
   input <- getInput
-  case input of
-    Up -> putStrLn "Yay, you won!"
-    _  -> main
-
-
-
-
-
-
-
-
-
-
-
-
+  print input
 
 
