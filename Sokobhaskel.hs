791761e395b8bbf83f48f7509696ab8527bf7baa
Simon Moos <simon.moos95@gmail.com>
2015-05-12 00:26:29 +0200

    Testing some stuff. The product is built on Raskell on my phone!

--- Sokobhaskel.hs
+++ Sokobhaskel.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Prelude hiding (Either(..))
 
+{-
 
 type Vec2 = (Int, Int)
 
@@ -58,11 +59,72 @@ showBoard = undefined
 readBoard :: String -> SokobanBoard
 readBoard = undefined
 
+-}
 
+type Coord = (Int, Int)
+
+data Level = Level { lvlSize :: Coord
+                   , lvlWalls :: [Coord]
+                   }
+                   deriving (Eq)
+
+instance Show Level where
+  show = showLevel
+
+setTile :: Level -> (Coord, Char) -> Level
+setTile lvl (coord, '#' ) = lvl{lvlWalls = coord:lvlWalls lvl}
+setTile lvl (_    , ' ' ) = lvl
+setTile _   (_    , char) = error (show char ++ " not recognized")
+
+getTile :: Level -> Coord -> Char
+getTile lvl coord | isWall lvl coord = '#'
+                  | otherwise        = ' '
+
+isWall :: Level -> Coord -> Bool
+isWall lvl coord = coord `elem` lvlWalls lvl
+
+emptyLevel :: Level
+emptyLevel = Level { lvlSize  = (0, 0)
+                   , lvlWalls = []
+                   }
+
+readLevel :: String -> Level
+readLevel str = foldl setTile (emptyLevel{lvlSize = maxi}) elems
+  where
+    lns     = lines str
+    coords  = [[(x,y) | x <- [0..]] | y <- [0..]]
+    elems   = concat $ zipWith zip coords lns
+    maxX    = maximum . map (fst . fst) $ elems
+    maxY    = maximum . map (snd . fst) $ elems
+    maxi    = (maxX, maxY)
+
+showLevel :: Level -> String
+showLevel lvl = unlines chars
+  where
+    (maxX, maxY) = lvlSize lvl
+    chars        = [[getTile lvl (x, y) | x <- [0..maxX]]
+                                        | y <- [0..maxY]]
 
 main :: IO ()
 main = do
-  input <- getInput
-  print input
+  levelContents <- readFile "level1.lvl"
+  let level = readLevel levelContents
+  print level
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
